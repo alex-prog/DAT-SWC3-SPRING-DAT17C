56dcd5bcc6932a322270905bde282010432f6757
Martin L. Jensen <martinloesethjensen@gmail.com>
2018-10-25 12:18:02 +0200

    Added a page with a list of cars.

---
+++ src/main/java/dk/kea/swc3/dat17c/demo/controller/CarController.java
@@ -0,0 +1,39 @@
+package dk.kea.swc3.dat17c.demo.controller;
+
+import dk.kea.swc3.dat17c.demo.model.Car;
+import dk.kea.swc3.dat17c.demo.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarController
+{
+	@GetMapping("/cars")
+	public String viewAllCars(Model model,
+	                          @RequestParam(defaultValue = "{{user}}")
+			                          String name,
+	                          @RequestParam(defaultValue = "-1")
+			                              Integer age,
+	                          @RequestParam(defaultValue = "F")
+			                              Character gender)
+	{
+		Car car1 = new Car("Ford", "blue", 5);
+		Car car2 = new Car("Volvo", "grey", 5);
+		Car car3 = new Car("Kia", "red", 5);
+		Car car4 = new Car("Mazda", "blue", 5);
+
+		List<Car> cars = new ArrayList<>();
+		cars.add(car1);
+		cars.add(car2);
+		cars.add(car3);
+		cars.add(car4);
+		model.addAttribute("cars", cars);
+		model.addAttribute("user", new User("Muddi", 21, 'F'));
+		return "list_of_cars";
+	}
+}
---
+++ src/main/java/dk/kea/swc3/dat17c/demo/model/Car.java
@@ -0,0 +1,45 @@
+package dk.kea.swc3.dat17c.demo.model;
+
+public class Car
+{
+	private String brand;
+	private String colour;
+	private int seats;
+
+	public Car(String brand, String colour, int seats)
+	{
+		this.brand = brand.toUpperCase();
+		this.colour = colour.toUpperCase();
+		this.seats = seats;
+	}
+
+	public String getBrand()
+	{
+		return brand;
+	}
+
+	public void setBrand(String brand)
+	{
+		this.brand = brand;
+	}
+
+	public String getColour()
+	{
+		return colour;
+	}
+
+	public void setColour(String colour)
+	{
+		this.colour = colour;
+	}
+
+	public int getSeats()
+	{
+		return seats;
+	}
+
+	public void setSeats(int seats)
+	{
+		this.seats = seats;
+	}
+}
