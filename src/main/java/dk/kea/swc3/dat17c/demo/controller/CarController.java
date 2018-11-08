b909fe6ff0d452030408afcb37c4f6a90f46dc07
alex-prog <coag@kea.dk>
2018-11-08 10:38:49 +0100

    JPA: save car working

--- src/main/java/dk/kea/swc3/dat17c/demo/controller/CarController.java
+++ src/main/java/dk/kea/swc3/dat17c/demo/controller/CarController.java
@@ -1,16 +1,22 @@
 package dk.kea.swc3.dat17c.demo.controller;
 
+import dk.kea.swc3.dat17c.demo.CarRepository;
 import dk.kea.swc3.dat17c.demo.model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 
 @Controller
 public class CarController {
+    @Autowired
+    private CarRepository carRepo;
+
 
     @GetMapping("/thisIsTheURLCar")
     public String car(Model model){
@@ -25,4 +31,22 @@ public class CarController {
         model.addAttribute("carsToBeSendToView", cars);
         return "car";
     }
+
+    @GetMapping("/car/add")
+    @ResponseBody
+    public String saveCar(
+            @RequestParam(defaultValue = "NO_BRAND")
+            String brand,
+            @RequestParam(defaultValue = "NO_COLOR")
+            String color,
+            @RequestParam(defaultValue = "-1")
+            Integer doors){
+
+        Car newCar = new Car(brand, color, doors);
+
+        carRepo.save(newCar);
+
+        return "OK";
+    }
+
 }
--- src/main/java/dk/kea/swc3/dat17c/demo/model/Car.java
+++ src/main/java/dk/kea/swc3/dat17c/demo/model/Car.java
@@ -1,6 +1,15 @@
 package dk.kea.swc3.dat17c.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String brand;
     private String color;
     private Integer doors;
